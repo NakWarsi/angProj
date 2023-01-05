82fd98106692e2d0b64185efd15b72729d55e2d1
nakwarsi <warsinak@gmail.com>
2023-01-05 12:14:53 +0530

    initial commit

--- companyProfile/Controllers/WeatherForecastController.cs
+++ companyProfile/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace companyProfile.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //it won't be needed
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
